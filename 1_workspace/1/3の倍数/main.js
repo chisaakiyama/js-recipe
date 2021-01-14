8ddd8b65d7d381fe62a15f7346c9101e74925bcd
chisaakiyama <chisa0426.age@gmail.com>
2021-01-14 19:30:20 +0900

    3の倍数発展1

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -11,3 +11,22 @@ const genkiFunction = function(number){
 }
 
 genkiFunction(24)
+
+const FizzBuzz = function(number){
+  for(let n=1; n<=number; n++){
+    if (n % 3 === 0 && n % 5 === 0) {
+      console.log("FizzBuzz")
+    }
+    else if (n % 5 === 0) {
+      console.log("Buzz")
+    }
+    else if (n % 3 === 0) {
+      console.log("Fizz")
+    }
+    else {
+      console.log(n)
+    }
+  }
+}
+
+FizzBuzz(50)
