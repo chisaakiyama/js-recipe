3b747285addb41eeee3412af27028f5c564b5581
chisaakiyama <chisa0426.age@gmail.com>
2021-01-19 15:01:08 +0900

    カウンターを２つにわけることができた

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -22,23 +22,30 @@ doubleButton.onclick = function(){
    display.textContent = count
  }
 
+ const display2 = document.getElementById("display2")
+ let count2 = 0
+
  const plusButton2 = document.getElementById("plus-button2")
  
  plusButton2.onclick = function(){
-   count += 1
-   display2.textContent = count
+   count2 += 1
+   display2.textContent = count2
  }
 
 const minusButton2 = document.getElementById("minus-button2")
 
 minusButton2.onclick = function(){
-  count -= 1
-  display2.textContent = count
+  count2 -= 1
+  display2.textContent = count2
 }
 
 const doubleButton2 = document.getElementById("double-button2")
 
 doubleButton2.onclick = function(){
-   count *= 2
-   display2.textContent = count
+   count2 *= 2
+   display2.textContent = count2
  }
+
+const answer = function(id){
+  return "display" + "display2"
+}
