ab989bc78dfdad5af964e0a05e75b1338b70b001
chisaakiyama <chisa0426.age@gmail.com>
2021-01-26 14:27:07 +0900

    便利なボタン＿完成

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -4,33 +4,33 @@ const plusButton = document.getElementById("plus-button")
 let count = 0
 
 plusButton.onclick = function(){
-   count += 1
-   display.textContent = count
- }
+  count += 1
+  display.textContent = count
+}
 
- const minusButton = document.getElementById("minus-button")
+const minusButton = document.getElementById("minus-button")
 
 minusButton.onclick = function(){
-   count -= 1
-   display.textContent = count
- }
+  count -= 1
+  display.textContent = count
+}
 
- const doubleButton = document.getElementById("double-button")
+const doubleButton = document.getElementById("double-button")
 
 doubleButton.onclick = function(){
-   count *= 2
-   display.textContent = count
- }
+  count *= 2
+  display.textContent = count
+}
 
- const display2 = document.getElementById("display2")
- let count2 = 0
+const display2 = document.getElementById("display2")
+let count2 = 0
 
- const plusButton2 = document.getElementById("plus-button2")
+const plusButton2 = document.getElementById("plus-button2")
  
- plusButton2.onclick = function(){
-   count2 += 1
-   display2.textContent = count2
- }
+plusButton2.onclick = function(){
+  count2 += 1
+  display2.textContent = count2
+}
 
 const minusButton2 = document.getElementById("minus-button2")
 
@@ -42,11 +42,6 @@ minusButton2.onclick = function(){
 const doubleButton2 = document.getElementById("double-button2")
 
 doubleButton2.onclick = function(){
-   count2 *= 2
-   display2.textContent = count2
-
- }
-
-const answer() = function(id){
-  return "display" + "display2"
+  count2 *= 2
+  display2.textContent = count2
 }
