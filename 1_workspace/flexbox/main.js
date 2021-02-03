48e5318bfc66bcbb6d7e8854ec72a865defe68ff
chisaakiyama <chisa0426.age@gmail.com>
2021-02-04 00:58:16 +0900

    写真表示されない

---
+++ 1_workspace/flexbox/main.js
@@ -0,0 +1,11 @@
+//マウスオーバー時の処理
+function mouseOn(){
+  var obj = document.getElementById("maria");
+  obj.src = "images/maria7_on.jpg";
+}
+  
+//マウスアウト時の処理
+function mouseOff(){
+  var obj = document.getElementById("maria");
+  obj.src = "images/maria7_off.jpg";
+}
